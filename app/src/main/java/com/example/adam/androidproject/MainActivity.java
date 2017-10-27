b5f8b75beced4ee1158b9bd51f67b405a681b5a4
vpetermio <vpetermio@yahoo.com>
2017-10-27 10:29:33 +0200

    added

--- app/src/main/java/com/example/adam/androidproject/MainActivity.java
+++ app/src/main/java/com/example/adam/androidproject/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.i("HELLO","HELLOWORLD");
         Log.i("HELLO","HELLOWORLD");
-        Log.i("HELLO","HELLOWORLD");
+        Log.i("HELLO","Peter");
 
     }
 
