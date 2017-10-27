e6eead4d49306845eb3f30e2f4efa947824afb15
BiancaSgondea <32032694+BiancaSgondea@users.noreply.github.com>
2017-10-27 10:30:45 +0200

    adeedD

--- app/src/main/java/com/example/adam/androidproject/MainActivity.java
+++ app/src/main/java/com/example/adam/androidproject/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("HELLO","HELLOWORLD");
-        Log.i("HELLO","HELLOWORLD");
+        Log.i("HELLO","Bianca");
         Log.i("HELLO","Peter");
 
     }
