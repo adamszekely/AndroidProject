15b5085fd8ccf2c8f3f00737cd8e9fd1c3094187
adamszekely <szekelyadam93@gmail.com>
2017-10-27 10:10:27 +0200

    Added

--- app/src/main/java/com/example/adam/androidproject/MainActivity.java
+++ app/src/main/java/com/example/adam/androidproject/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("HELLO","HELLOWORLD");
+
+        Log.i("HELLOWORLD", "WHAT'S UP?");
     }
 
 }
