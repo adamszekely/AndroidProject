9f5df68e2515a6d72ad8f0027d5fb3eaee0c8b54
adamszekely <szekelyadam93@gmail.com>
2017-10-27 10:35:01 +0200

    Deleted

--- app/src/main/java/com/example/adam/androidproject/MainActivity.java
+++ app/src/main/java/com/example/adam/androidproject/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("HELLO","HELLOWORLD");
+        Log.i("HELLO","PEter");
         Log.i("HELLO","Bianca");
         Log.i("HELLO","ADam");
 
