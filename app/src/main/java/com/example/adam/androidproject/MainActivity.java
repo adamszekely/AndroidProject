6a28f0b9bffbc8b6ce0a6dea4ce34a09eeacbdc9
Tomas Cachojanas <cikatom@gmail.com>
2017-11-01 15:26:42 +0100

    Login layout

--- app/src/main/java/com/example/adam/androidproject/MainActivity.java
+++ app/src/main/java/com/example/adam/androidproject/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.adam.androidproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.adam.androidproject.Login.Login;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       
+
+         loginButton = (Button)findViewById(R.id.loginButton);
+    loginButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            Intent intent = new Intent(MainActivity.this,Login.class);
+            startActivity(intent);
+        }
+    });
     }
 
 }
